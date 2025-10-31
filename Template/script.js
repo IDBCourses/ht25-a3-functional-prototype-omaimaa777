0ad3946e2254be2e539195ce688efcebf24b9c8a
Omaimaa Nageeb <182335521+omaimaa777@users.noreply.github.com>
2025-10-31 12:42:58 +0100

    wrote constant objs that dont change during the game

---
+++ Template/script.js
@@ -0,0 +1,9 @@
+
+// Settings - these never change during the game (constant object)
+
+const settings = {
+  targetZoneHeight: 60, //How tall the target zone is in px
+  leftKeys: ["q", "w", "e", "a", "s", "d"], // array of the left lane keys
+  middleKeys: ["r", "t", "f","g"], // array of the middle lane keys
+  rightKeys: ["y", "u", "h", "j", "n", "m"] //array of the right lane keys
+};
